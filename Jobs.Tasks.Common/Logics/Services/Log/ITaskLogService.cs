b3ebcef9cf67221b6e7610ac19bcadf6af1d7bd2
b.salimov <bunyod545@mail.ru>
2020-04-01 11:09:39 +0500

    Jobs view continue

---
+++ Jobs.Tasks.Common/Logics/Services/Log/ITaskLogService.cs
@@ -0,0 +1,32 @@
+﻿namespace Jobs.Tasks.Common.Logics.Services.Log
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ITaskLogService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        void Success(string msg);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        void Information(string msg);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        void Warning(string msg);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        void Error(string msg);
+    }
+}

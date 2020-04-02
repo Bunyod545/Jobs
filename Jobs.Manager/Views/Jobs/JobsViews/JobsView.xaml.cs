cfe48750d7f86c24ecd02a03e5c9aa7fa3008e36
b.salimov <bunyod545@mail.ru>
2020-04-02 10:00:14 +0500

    merged

--- Jobs.Manager/Views/Jobs/JobsViews/JobsView.xaml.cs
+++ Jobs.Manager/Views/Jobs/JobsViews/JobsView.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using Jobs.Common.Database;
 using Jobs.Common.Database.Tables;
+using Jobs.Manager.Views.Jobs.JobsViews.Models;
 using Jobs.Manager.Views.Tasks;
 
 namespace Jobs.Manager.Views.Jobs
@@ -27,7 +28,7 @@ namespace Jobs.Manager.Views.Jobs
             JobGroupsTree.SelectedItemChanged += JobGroupsTreeOnSelectedItemChanged;
 
             ViewModel = new JobsViewModel();
-            ViewModel.JobGroups = new ObservableCollection<JobGroup>();
+            ViewModel.JobGroups = new ObservableCollection<JobGroupInfo>();
 
             DataContext = ViewModel;
         }
@@ -39,12 +40,11 @@ namespace Jobs.Manager.Views.Jobs
         /// <param name="e"></param>
         private void JobGroupsTreeOnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            var group = e.NewValue as JobGroup;
+            var group = e.NewValue as JobGroupInfo;
             if (group == null)
                 return;
 
             ViewModel.SelectedJobGroup = group;
-            ViewModel.Jobs = new ObservableCollection<Job>(group.Jobs);
         }
 
         /// <summary>

67fabc2332faec6728d877334df6b23557c946b4
b.salimov <bunyod545@mail.ru>
2020-04-02 10:13:14 +0500

    Jobs.Tasks.Common ITaskCustomizedLogService

---
+++ Jobs.Tasks.Common/Logics/Services/Log/ITaskCustomizedLogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Jobs.Tasks.Common.Logics.Services.Log
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ITaskCustomizedLogService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logAction"></param>
+        void Show(Action<RichTextBox> logAction);
+    }
+}

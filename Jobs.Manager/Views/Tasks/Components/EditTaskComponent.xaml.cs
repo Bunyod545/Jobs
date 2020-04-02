4492f71e5120f0ce91962a54649e5fc391237792
Bohodir Zoitov <2@DESKTOP-2HQNUGT>
2020-04-02 11:03:17 +0500

    Jobs.Manager added EditTaskComponent

---
+++ Jobs.Manager/Views/Tasks/Components/EditTaskComponent.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Jobs.Manager.Views.Tasks.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class EditTaskComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public readonly EditTaskComponentModel ViewModel;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public EditTaskComponent()
+        {
+            InitializeComponent();
+            ViewModel = new EditTaskComponentModel();
+            DataContext = ViewModel;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public EditTaskComponent(EditTaskComponentModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = ViewModel;
+        }
+    }
+}
---
+++ Jobs.Manager/Views/Tasks/Components/EditTaskComponentModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Jobs.Manager.Annotations;
+
+namespace Jobs.Manager.Views.Tasks.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EditTaskComponentModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsChecked { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
